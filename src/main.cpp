e781adefedcdbfad67701f922fd3a5b602c66f1a
Woojin Son <75523802+ichbinderwooj@users.noreply.github.com>
2022-03-22 19:10:10 +0100

    Added some input scanning

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
-int main() {}
+#include <cstdio>
+
+int main()
+{
+    float mass, drag_coefficient, air_density, area;
+
+    printf("Mass: ");
+    scanf_s("%f", &mass);
+    printf("Drag Coefficient: ");
+    scanf_s("%f", &drag_coefficient);
+    printf("Air density: ");
+    scanf_s("%f", &air_density);
+    printf("Cross-sectional Area: ");
+    scanf_s("%f", &area);
+
+    return 0;
+}
